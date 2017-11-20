435aad104840c4f0ade770a6faa3825b00a6c492
rivermoondw <ztblueskytz@gmail.com>
2017-11-20 08:35:19 +0700

    Revert "add server"
    This reverts commit d3d349cbe47ddfa25b7d0a14c7b0e644e965b88c.

--- tintuc/app/models/admin/m_category.js
+++
@@ -1,6 +0,0 @@
-var db = require('../../../lib/db.js');
-var main_table = 'article_categories';
-exports.add = function(data) {
-	data.order_sort = parseInt(data.order_sort);
-	db.insert(main_table, data);
-}
